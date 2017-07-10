4ddaae890c325849a3d3e5755d34e2a1af19c550
Anthony Frugier <fru.anthony@gmail.com>
2017-07-10 10:59:03 +0200

    test 01

--- src/test/java/java8/ex01/Exemple_Test.java
+++ src/test/java/java8/ex01/Exemple_Test.java
@@ -8,11 +8,19 @@ import org.junit.Test;
  * Exemple Test
  */
 public class Exemple_Test {
-	
+
 	@Test
 	public void testQuiEchoue() {
 		fail();
 	}
 
-  
+	@Test
+	public void test1() {
+		int nb1 = 12;
+		int nb2 = 24;
+
+		int somme = nb1 + nb2;
+		assert 36 == somme;
+	}
+
 }
